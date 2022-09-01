a57be47b60a17a82ff427d29a92cf87136fa7c64
voromax <voromax@gmail.com>
2022-09-01 09:00:27 +0300

    Added feature test file

---
+++ tests/test_feature.py
@@ -0,0 +1,55 @@
+import pytest
+from requests_html import HTMLSession, AsyncHTMLSession, HTMLResponse
+
+#
+# 
+#  By now this is a copy of some test_
+#  I hope it will become a test of some request-response scenarios in the future 
+#
+#
+
+session = HTMLSession()
+
+@pytest.mark.internet
+def test_pagination():
+    pages = (
+        'https://xkcd.com/1957/',
+        'https://smile.amazon.com/',
+        'https://theverge.com/archives'
+    )
+
+    for page in pages:
+        r = session.get(page)
+        assert next(r.html)
+
+@pytest.mark.internet
+@pytest.mark.asyncio
+async def test_async_pagination(event_loop):
+    asession = AsyncHTMLSession()
+    pages = (
+        'https://xkcd.com/1957/',
+        'https://smile.amazon.com/',
+        'https://theverge.com/archives'
+    )
+
+    for page in pages:
+        r = await asession.get(page)
+        assert await r.html.__anext__()
+
+@pytest.mark.internet
+def test_async_run():
+    asession = AsyncHTMLSession()
+
+    async def test1():
+        return await asession.get('https://xkcd.com/1957/')
+
+    async def test2():
+        return await asession.get('https://reddit.com/')
+
+    async def test3():
+        return await asession.get('https://smile.amazon.com/')
+
+    r = asession.run(test1, test2, test3)
+
+    assert len(r) == 3
+    assert isinstance(r[0], HTMLResponse)
